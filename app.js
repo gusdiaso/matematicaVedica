1162cbc4e865f311abfd30e63c570a19653583c5
gusdiasx <oliveira.gustavo_4@graduacao.uerj.br>
2022-07-20 11:03:55 -0300

    Validacao adicionada

--- app.js
+++ app.js
@@ -1,86 +1,78 @@
+//_____________ DECLARAÇÃO _____________//
+
+let operador = undefined
+
 const input = document.getElementById('input'),
-    botao_soma = document.getElementById('button_soma'),
-    botao_sub = document.getElementById('button_sub'),
-    botao_mult = document.getElementById('button_mult'),
-    botao_div = document.getElementById('button_div'),
-    botao_0 = document.getElementById('button_0'),
-    botao_1 = document.getElementById('button_1'),
-    botao_2 = document.getElementById('button_2'),
-    botao_3 = document.getElementById('button_3'),
-    botao_4 = document.getElementById('button_4'),
-    botao_5 = document.getElementById('button_5'),
-    botao_6 = document.getElementById('button_6'),
-    botao_7 = document.getElementById('button_7'),
-    botao_8 = document.getElementById('button_8'),
-    botao_9 = document.getElementById('button_9'),
     button_calcular = document.querySelector('.button_calcular')
 
-{
-    //botoes para input
-    button_soma.addEventListener('click', () => {
-        input.value += '+'
-    })
+//_______________ CODIGO _______________//
 
-    button_sub.addEventListener('click', () => {
-        input.value += '-'
-    })
+button_calcular.addEventListener('click', () => {
+    const stringSeparada = separaString(input.value)
+    if (stringSeparada[stringSeparada.length - 1] == '') {
+        stringSeparada.pop()
+    }
+    if (input.value == '') {
+        alert('Nada foi digitado')
+    }
+    console.log(stringSeparada)
+})
 
-    button_mult.addEventListener('click', () => {
-        input.value += '*'
-    })
-
-    button_div.addEventListener('click', () => {
-        input.value += '/'
-    })
-
-    button_0.addEventListener('click', () => {
-        input.value += '0'
-    })
-
-    button_1.addEventListener('click', () => {
-        input.value += '1'
-    })
-
-    button_2.addEventListener('click', () => {
-        input.value += '2'
-    })
-
-    button_3.addEventListener('click', () => {
-        input.value += '3'
-    })
-
-    button_4.addEventListener('click', () => {
-        input.value += '4'
-    })
-
-    button_5.addEventListener('click', () => {
-        input.value += '5'
-    })
-
-    button_6.addEventListener('click', () => {
-        input.value += '6'
-    })
-
-    button_7.addEventListener('click', () => {
-        input.value += '7'
-    })
-
-    button_8.addEventListener('click', () => {
-        input.value += '8'
-    })
-
-    button_9.addEventListener('click', () => {
-        input.value += '9'
-    })
-
-    button_calcular.addEventListener('click', () => {
-        separador = '+'
-        const stringSeparada = separaString(input.value, separador)
-        console.log(stringSeparada)
-    })
+function separaString(stringParaSeparar) {
+    let separadorSelecionado = pegaOperador(stringParaSeparar)
+    //pega cada numero separado pelo operador
+    return stringParaSeparar.split(separadorSelecionado)
 }
 
-function separaString(stringParaSeparar, separador) {
-    const vetorString = stringParaSeparar.split(separador)
-    return vetorString
+function pegaOperador(string) {
+    const separador = ['+', '-', '*', '/']
+    let vetorCaracter = string.split('')
+    let separadorSelecionado
+
+    //pega operador
+    for (let i = 0; i < vetorCaracter.length; i++) {
+        if (separador.includes(vetorCaracter[i])) {
+            separadorSelecionado = vetorCaracter[i]
+            console.log(separadorSelecionado)
+        }
+    }
+    return separadorSelecionado
+}
+
+function insereInput(valor) {
+    const inicialProibido = ['0', '+', '-', '*', '/']
+    const numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    const arrayCaracter = input.value.split('')
+
+    //pega variavel glogal operador e define o primeiro operador inserido como operador "único"
+    if (operador == undefined) {
+        operador = pegaOperador(input.value)
+    }
+
+    //validação
+
+    //primeiro input nao pode ser ['0', '+', '-', '*', '/']
+    if (input.value.length == 0 && inicialProibido.includes(valor)) {
+        alert('Operação não pode começar com um zero ou com operadores!')
+    }
+    //pós operador o input nao pode ser ['0', '+', '-', '*', '/']
+    else if (
+        arrayCaracter[arrayCaracter.length - 1] == operador &&
+        inicialProibido.includes(valor)
+    ) {
+        alert('Não insira um zero ou operador após outro operador!')
+    }
+    //proibir entrada de outro operador sem ser o operador unico
+    else if (
+        valor != operador &&
+        operador != undefined &&
+        !numeros.includes(valor)
+    ) {
+        alert('Utilize apenas o primeiro operador escolhido!')
+        console.log(operador)
+    }
+    //outros casos sao permitidos
+    else {
+        input.value += valor
+    }
 }
